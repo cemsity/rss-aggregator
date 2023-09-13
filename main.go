1f539d65d5731cf0a215c8af786ef9fb3c43e663
Stephen Hilson <cemsity@gmail.com>
2023-09-13 11:36:17 -0400

    Add  and

--- main.go
+++ main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	genv "github.com/joho/godotenv"
+	"github.com/go-chi/chi/v5"
 )
 
 func main(){
 	fmt.Println("HELLO WORLD")
 	fmt.Println("TESTING!!!")
+	genv.Load(".env")
+	
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		log.Fatal("PORT is not found in the environment")
+	}
+	fmt.Println(port)
+
+	router := chi.NewRouter()
+	
 }
