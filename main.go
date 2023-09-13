48c341963453170c4ad8ddafb2ab7573712f01d1
Stephen Hilson <cemsity@gmail.com>
2023-09-13 12:10:26 -0400

    feat: add `/v1` routes
    feat: add `/healthz` as health check endpoint
    feat: add `/error` as POC for error handling

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
-	genv "github.com/joho/godotenv"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	genv "github.com/joho/godotenv"
 )
 
 func main(){
@@ -21,5 +24,29 @@ func main(){
 	fmt.Println(port)
 
 	router := chi.NewRouter()
-	
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/error", handlerErr)
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":" + port,
+	}
+
+	log.Printf("Server starting on port %v", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
